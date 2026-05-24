3e52cae8a07e9145e758cf62c4b8235e789924bc
tsr0705 <tanmaysingh8246@gmail.com>
2026-05-24 13:52:18 +0530

    fix(user): update user model schema

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,6 +1,7 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { env } from "../config/env.js";
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -9,29 +10,29 @@ const userSchema = new mongoose.Schema({
         unique: true,
         trim: true,
         lowercase: true,
-        minLength: [ 6, 'Email must be at least 6 characters long' ],
-        maxLength: [ 50, 'Email must be at most 50 characters long' ]
+        minLength: [6, "Email must be at least 6 characters long"],
+        maxLength: [50, "Email must be at most 50 characters long"],
     },
 
     password: {
         type: String,
         required: true,
-        select: false
-    }
-})
+        select: false,
+    },
+});
 
 userSchema.statics.hashPassword = async function (password) {
     return await bcrypt.hash(password, 10);
-}
+};
 
 userSchema.methods.comparePassword = async function (password) {
     return await bcrypt.compare(password, this.password);
-}
+};
 
 userSchema.methods.generateJWT = function () {
-    return jwt.sign({ email: this.email }, process.env.JWT_SECRET, { expiresIn: '24h' });
-}
+    return jwt.sign({ _id: this._id, email: this.email }, env.JWT_SECRET, { expiresIn: "24h" });
+};
 
-const User = mongoose.model('user', userSchema);
+const User = mongoose.model("user", userSchema);
 
 export default User;
