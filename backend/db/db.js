23de75c6064dd98293db8749b73ad7c1b7c3be6b
tsr0705 <tanmaysingh8246@gmail.com>
2026-05-24 13:51:45 +0530

    fix(db): update DB connection/queries

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
-
+import { env } from "../config/env.js";
 
 function connect() {
-    mongoose.connect(process.env.MONGODB_URI)
+    mongoose
+        .connect(env.MONGODB_URI)
         .then(() => {
             console.log("Connected to MongoDB");
         })
-        .catch(err => {
-            console.log(err);
-        })
+        .catch((err) => {
+            console.error("MongoDB connection error:", err.message);
+        });
 }
 
 export default connect;
